65758c7c4cbb395924b79a3fd5e3d484664fd8a6
Marcelot <marcelojtelles@gmail.com>
2019-04-06 22:05:53 -0300

    Add files via upload

---
+++ 4.sh
@@ -0,0 +1,7 @@
+gpio -g write 21 0
+gpio -g write 20 1
+gpio -g write 16 1
+gpio -g write 26 0
+gpio -g write 6 1
+gpio -g write 13 1
+gpio -g write 19 0
---
+++ 7.sh
@@ -0,0 +1,7 @@
+gpio -g write 21 1
+gpio -g write 20 1
+gpio -g write 16 1
+gpio -g write 26 0
+gpio -g write 6 1
+gpio -g write 13 0
+gpio -g write 19 0
